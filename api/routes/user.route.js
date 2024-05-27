4aeb849ca2f4f876d3db7d37c2ddb7ab0bc8d74c
Fres02 <133979643+Fres02@users.noreply.github.com>
2024-05-27 10:45:56 +0530

    create sign up api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,8 @@
 import express from 'express';
+import {test} from "../controllers/user.controller.js"
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/test', (req,res)=>{
-    res.json({
-        message:'API Route is working',
-    }
-    )
-}
-);
+router.get('/test', test);
 
 export default router;
