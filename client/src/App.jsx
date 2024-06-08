d21590e379b806f510bb88470bc41a9be78912dc
Fres02 <133979643+Fres02@users.noreply.github.com>
2024-06-08 23:47:35 +0530

    crete listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import CreateListing from './pages/CreateListing';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -16,7 +17,8 @@ export default function App() {
     <Route path="/sign-up" element={<SignUp />} />
     <Route path="/about" element={<About />} />
     <Route element = {<PrivateRoute/>}>
-      <Route path="/profile" element={<Profile />} />      
+      <Route path="/profile" element={<Profile />} />  
+      <Route path="/create-listing" element={<CreateListing />} />           
     </Route>
 
   </Routes>
