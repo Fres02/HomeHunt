46087a1b1eb176253ec393555862a7633ee63fcc
Fres02 <133979643+Fres02@users.noreply.github.com>
2024-07-18 02:48:45 +0530

    search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -61,4 +61,63 @@ export const createListing = async (req, res, next) => {
   }catch(error){
     next(error);
   }
-  }
+  };
+
+  export const getListings = async (req, res, next) => {
+    try {
+      const limit = parseInt(req.query.limit) || 6;
+      const startIndex = parseInt(req.query.startIndex) || 0;
+      let offer = req.query.offer;
+  
+      if (offer === undefined || offer === 'false') {
+        offer = { $in: [false, true] };
+      }
+  
+      let furnished = req.query.furnished;
+  
+      if (furnished === undefined || furnished === 'false') {
+        furnished = { $in: [false, true] };
+      }
+  
+      let parking = req.query.parking;
+  
+      if (parking === undefined || parking === 'false') {
+        parking = { $in: [false, true] };
+      }
+  
+      let type = req.query.type;
+  
+      if (type === undefined || type === 'all') {
+        type = { $in: ['sale', 'rent'] };
+      }
+
+      let propertyType = req.query.type;
+  
+      if (propertyType === undefined || propertyType === 'House') {
+        propertyType = { $in: ['House'] };
+      }
+
+  
+      const searchTerm = req.query.searchTerm || '';
+  
+      const sort = req.query.sort || 'createdAt';
+  
+      const order = req.query.order || 'desc';
+  
+      const listings = await Listing.find({
+        name: { $regex: searchTerm, $options: 'i' },
+        offer,
+        furnished,
+        parking,
+        type,
+        propertyType,
+      })
+        .sort({ [sort]: order })
+        .limit(limit)
+        .skip(startIndex);
+  
+      return res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  };
