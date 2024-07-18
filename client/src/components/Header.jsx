2b6098eae69e0782b5115f9d6028623bf9f29f4a
Fres02 <133979643+Fres02@users.noreply.github.com>
2024-07-18 17:37:16 +0530

    complete header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,26 @@ import logo from '../assets/images/1.png';
 
 
 export default function Header() {
-    const {currentUser} = useSelector(state=>state.user)
+    const {currentUser} = useSelector(state=>state.user);
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e)=>{
+      e.preventDefault();
+      const urlParams = new URLSearchParams(window.location.search);
+      urlParams.set('searchTerm', searchTerm);
+      const searchQuery = urlParams.toString();
+      navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFromUrl = urlParams.get('searchTerm');
+      if (searchTermFromUrl) {
+        setSearchTerm(searchTermFromUrl);
+      }
+    }, [location.search]);
+
     
     return (
 
@@ -21,8 +40,8 @@ export default function Header() {
 
       <ul className='flex gap-6 items-center'>
         <li>      
-          <form className='relative'>
-            <input type="text" placeholder="Search..." className='bg-blue-50 focus:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 w-20 sm:w-40 px-4 py-2 rounded-full transition duration-200'/>
+          <form onSubmit={handleSubmit} className='relative'>
+            <input type="text" placeholder="Search..." className='bg-blue-50 focus:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 w-20 sm:w-40 px-4 py-2 rounded-full transition duration-200' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)}/>
             <button type="submit" className='absolute right-2 top-1/2 transform -translate-y-1/2 text-indigo-600'>
             <FaSearch />
             </button>
